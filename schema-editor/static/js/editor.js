8017d72dcb6e2c2c3f4bc1c6d8b79f7944a68260
Ben Weaver <weaver@coptix.com>
2010-03-30 19:51:20 -0400

    schema-editor

---
+++ schema-editor/static/js/editor.js
@@ -0,0 +1,172 @@
+(function($) {
+    var _body,
+        _bd,
+        _form,
+        _fields,
+        _field,
+        _drag,
+        _avro,
+        _below_avro;
+
+    $(document).ready(function() {
+        init_fields();
+        init_schema();
+        init_controls();
+    });
+
+    // ---------- Controls ----------
+
+    function init_controls() {
+        $('#add-field').click(add_field);
+        $('#show-schema').click(show_schema);
+        $('#show-editor').click(show_editor);
+
+        _body = $('body');
+        _bd = $('#bd');
+        _below_avro = $('#avro').height() - _avro.height();
+    }
+
+    function show_schema() {
+        _avro.height(_bd.height() - _below_avro);
+        _form.animate({
+            'margin-left': '-' + _form.outerWidth() + 'px'
+        }, 'fast', 'swing');
+    }
+
+    function show_editor() {
+        _form.animate({
+            'margin-left': '0px'
+        }, 'fast', 'swing');
+    }
+
+    // ---------- Field List ----------
+
+    function init_fields() {
+        _fields = $('#fields');
+
+        _field = _fields.children('.template')
+            .remove().removeClass('template')
+            .attr('draggable', 'true');
+
+        _fields.data('item-template', _field);
+
+        _fields
+            .bind('click', click_field)
+            .bind('dragstart', drag_start)
+            .bind('dragend', drag_end)
+            .bind('dragenter', drag_enter)
+            .bind('dragover', drag_over)
+            .bind('drop', drop);
+    }
+
+    function add_field() {
+        var field = _field.clone().appendTo(_fields);
+        _body.attr('scrollTop', _body.attr('scrollHeight'));
+        _form.change();
+    }
+
+    function remove_field(field) {
+        field.remove();
+        _form.change();
+    }
+
+    function click_field(ev) {
+        var target = $(ev.target);
+        if (target.is('.remove')) {
+            remove_field(target.parents('.field'));
+        }
+    }
+
+    function field(obj) {
+        return $(obj).up('.field');
+    }
+
+    function drag_start(ev) {
+        _drag = $(ev.target).addClass('drag');
+        ev.originalEvent.dataTransfer.setData('FireFox', 'requires this');
+        return true;
+    }
+
+    function drag_end(ev) {
+        $(ev.target).removeClass('drag')
+            .siblings().andSelf().removeClass('over');
+    }
+
+    function drag_enter(ev) {
+        field(ev.target)
+            // Do this here instead of dragleave because of the order
+            // WebKit fires events for nested elements.
+            .siblings('.over').removeClass('over').end()
+            .addClass('over');
+    }
+
+    function is_drop(elem) {
+        return (elem != _drag[0]) && (elem.parentNode == _drag[0].parentNode);
+    }
+
+    function drag_over(ev) {
+        // Return "false" if dropping is allowed.
+        return !is_drop(field(ev.target).get(0));
+    }
+
+    function drop(ev) {
+        // Place the dragged node on the "other side" of the drop
+        // target depending on their relative position.
+        var drop = field(ev.target).removeClass('over');
+        drop[(drop.index() > _drag.index()) ? 'after' : 'before'](_drag);
+        _drag = undefined;
+        _form.change();
+        return false;
+    }
+
+    // ---------- Editor / Textarea ----------
+
+    function init_schema() {
+        _form = $('#record').change(on_editor_change);
+        _avro = $('#avro textarea').change(on_schema_change);
+        _avro.change();
+    }
+
+    function on_editor_change(ev) {
+        store(_form.value());
+    }
+
+    function on_schema_change(ev) {
+        store(update_editor(load(_avro.val())));
+    }
+
+    function load(data) {
+        try {
+            return JSON.parse(data);
+        }
+        catch(exn) {
+            alert('Load Error: ' + exn);
+            throw exn;
+        }
+    }
+
+    function store(obj) {
+        _avro.val(JSON.stringify(obj, undefined, 2));
+    }
+
+    function update_editor(obj) {
+        _form.value(obj);
+        return obj;
+    }
+
+    // The value of a maybe-list may be:
+    //   "", "foo", or ["foo", "bar", ...]
+    $.fn.value.get('maybe-list', function(input) {
+        var val = input.val();
+        if (!val || val.length == 0) {
+            return "";
+        }
+        else if (val.length == 1) {
+            return val[0];
+        }
+        else {
+            return val;
+        }
+    });
+
+})(jQuery);
---
+++ schema-editor/static/js/role.js
@@ -0,0 +1,177 @@
+(function($) {
+
+    // Starting with the current query, try to match the selector
+    // otherwise try parent().
+    $.fn.up = function(sel) {
+        var item = this;
+        while (item && !item.is(sel)) {
+            item = item.parent();
+        }
+        return item;
+    };
+
+    // down() -- a shallow find/each
+    //
+    // Walk downward, until the selector is matched.  Call fn() on
+    // matched each matched item.  Don't traverse into matched item.
+    $.fn.down = function(sel, fn) {
+        var queue = this.get(),
+            elem, item, idx, lim;
+
+        while (queue.length > 0) {
+            elem = queue.shift();
+            item = $(elem);
+            if (!item.is(sel))
+                $.merge(queue, item.children());
+            else if (false === fn.call(queue[idx], item))
+                break;
+        }
+
+        return this;
+    };
+
+    // Return the name of the first query item.
+    $.fn.name = function() {
+        return this.attr('name') || this.attr('data-name');
+    };
+
+    // Add a ":named" pseudo-selector that works like
+    $.expr[':'].named = function(elem) {
+        return !!(elem.getAttribute('name')
+                  || elem.getAttribute('data-name'));
+    };
+
+    $.fn.value = function(value) {
+        if (value === undefined)
+            return getval(this);
+        else if (this.length < 2)
+            return setval(this, value);
+        return this.each(function(_, item) {
+            setval($(item), value);
+        });
+    };
+
+    // defrole() -- define a generic role-method over a WAI-ARIA role.
+    //
+    // A role is a space-separated list of types from most specific to
+    // least specific.  A role-method is a registry of methods for
+    // individual types.  When it is called on an element, the most
+    // specific role-method that matches the element is called.
+    $.defrole = function(registry) {
+        var cache;
+
+         registry = registry || {};
+         if ($.isFunction(registry)) {
+             registry = { undefined: registry };
+         }
+
+        // Keep a cache of roles that aren't defined directly in the
+        // registry.  When a cache miss occurs, the role is scanned
+        // for the most specific match by dispatch().
+        function Cache() {}
+        Cache.prototype = registry;
+
+        // Clear the cache.
+        function clear() {
+            cache = new Cache();
+        }
+
+        // The dispatch method is returned by defrole().  It tries to
+        // look up the role in the cache and falls back to scanning.
+        function dispatch(elem) {
+            var role = elem.attr('role');
+            if (role === undefined)
+                return cache[undefined].apply(cache, arguments);
+
+            var probe = cache[role];
+            if (!probe) {
+                $.each(role.split(/\s+/), function(_, key) {
+                    probe = cache[key];
+                    return probe !== undefined;
+                });
+                probe = cache[role] = probe || cache[undefined];
+            }
+            return probe.apply(cache, arguments);
+        };
+
+        // A define() method is attached to the dispatcher for future
+        // extension.  The cache is cleared in case a more specific
+        // method has been defined.
+        dispatch.define = function(key, op) {
+            clear();
+            registry[key] = op;
+            return op;
+        };
+
+        clear(); // Prime the cache.
+        return dispatch;
+    };
+
+    // getval() -- create a JSON value from a structured set of form
+    // inputs.
+    var getval = $.defrole({
+        undefined: function(query) {
+            return query.is(':input') ? query.val() : this.object(query);
+        },
+
+        object: function(query) {
+            var obj = {};
+
+            query.down(':named', function(input) {
+                obj[input.name()] = getval(input);
+            });
+
+            return obj;
+        },
+
+        list: function(input) {
+            var result = [];
+            input.children().each(function(idx, item) {
+                result.push(getval($(item)));
+            });
+            return result;
+        }
+    });
+
+    // setval() -- set the value of a structured input using a JSON
+    // object.
+    var setval = $.defrole({
+        undefined: function(query, obj) {
+            return query.is(':input') ? query.val(obj) : this.object(query, obj);
+        },
+
+        object: function(query, obj) {
+            var inputs = map_named(query);
+
+            $.each(obj, function(key, val) {
+                if (key in inputs)
+                    setval(inputs[key], val);
+            });
+
+            return query;
+        },
+
+        list: function(input, obj) {
+            // FIXME: hacky
+            var tpl = input.empty().data('item-template');
+            $.each(obj, function(index, field) {
+                input.append(setval(tpl.clone(), field));
+            });
+            return input;
+        }
+    });
+
+    // Make a mapping of {name: input} :named items that are down()
+    // from query.
+    function map_named(query) {
+        var result = {};
+        query.down(':named', function(input) {
+            result[input.name()] = input;
+        });
+        return result;
+    }
+
+    $.fn.value.get = getval.define;
+    $.fn.value.set = setval.define;
+
+})(jQuery);
