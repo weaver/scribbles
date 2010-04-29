47908dd35f410f790e518cf5bcc22f3b0bbfca3a
Ben Weaver <weaver@coptix.com>
2010-04-29 18:12:53 -0400

    Added node.js telnet client.

---
+++ telnet/README.md
@@ -0,0 +1,8 @@
+# telnet.js #
+
+This is a simple telnet client implemented with [node.js][0].  Use it
+like this:
+
+    node telnet.js irc.freenode.net 6667
+
+[0]: http://nodejs.org/
---
+++ telnet/telnet.js
@@ -0,0 +1,37 @@
+// telnet.js -- a simple telnet client
+//
+// > node telnet.js irc.freenode.net 6667
+
+var net = require('net'),
+    sys = require('sys'),
+    argv = process.argv;
+
+if (argv.length != 4) {
+    sys.puts('usage: ' + argv[0] + ' ' + argv[1] + ' HOST PORT');
+    process.exit(1);
+}
+
+(function(host, port) {
+     var name = host + ':' + port,
+         client = net.createConnection(port, host);
+
+     client.addListener('connect', function() {
+         var input = process.openStdin(),
+             output = process.stdout;
+
+         sys.puts('Connected to ' + name + '; use ^C to exit.');
+
+         input.addListener('data', function(chunk) {
+             client.write(chunk);
+         });
+
+         client.addListener('data', function(chunk) {
+             output.write(chunk);
+         });
+
+         client.addListener('close', function() {
+             sys.puts('Connection to ' + name + ' closed.');
+             process.exit(0);
+         });
+    });
+}).apply(this, argv.slice(2));
